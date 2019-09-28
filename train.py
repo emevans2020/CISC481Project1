c3f47007f1c1b29dc996c4725a9edfc6bc80fd72
Emily Evans <emilyevans@Emilys-MacBook-Pro-2.local>
2019-09-28 19:23:51 -0400

    working on it

--- train.py
+++ train.py
@@ -3,8 +3,17 @@
 # List of points that are mapped to each other. 
 # Our goal is to get from any point to the goal point, in this case 5
 
-yard = [(1,2),(1,3),(3,5),(4,5),(2,6),(5,6)]  
-
+# yard1 = [(1,2),(1,3),(3,5),(4,5),(2,6),(5,6)]  dont think about testing yard 1 until you figure out yards 3,4,5
+yard3 = [(1,2), (1,3)]
+yard4 = [(1,2), (1,3), (1,4)]
+(defvar *INIT-STATE-3* ’((*) (a) (b)))
+(defvar *GOAL-STATE-3* ’((* a b) nil nil))
+(defvar *YARD-4* ’((1 2) (1 3) (1 4)))
+(defvar *INIT-STATE-4* ’((*) (a) (b c) (d)))
+(defvar *GOAL-STATE-4* ’((* a b c d) nil nil nil))
+(defvar *YARD-5* ’((1 2) (1 3) (1 4)))
+(defvar *INIT-STATE-5* ’((*) (a) (c b) (d))) ;Note c and b out of order
+(defvar *GOAL-STATE-5* ’((* a b c d) nil nil nil))
 
 function UNIFORM-COST-SEARCH(problem) returns a solution, or failure
 node ← a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
