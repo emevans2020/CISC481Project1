475c98869159582989dbf21faa063a123c09147e
Emily Evans <emilyevans@Emilys-MacBook-Pro-2.local>
2019-09-28 18:06:34 -0400

    starting files using python

---
+++ train.py
@@ -0,0 +1,6 @@
+# Emily Evans and Sasha Jones			Due: September 28
+
+# List of points that are mapped to each other. 
+# Our goal is to get from any point to the goal point, in this case 5
+
+environment = [(0,4),(4,3),(2,3),(1,3),(4,5),(1,5)]
