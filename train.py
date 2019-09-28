51298ed1b568615510de50cfacfa1090850d72ce
Emily Evans <emilyevans@Emilys-MacBook-Pro-2.local>
2019-09-28 18:11:38 -0400

    working

--- train.py
+++ train.py
@@ -3,4 +3,5 @@
 # List of points that are mapped to each other. 
 # Our goal is to get from any point to the goal point, in this case 5
 
-environment = [(0,4),(4,3),(2,3),(1,3),(4,5),(1,5)]
+yard = [(1,2),(1,3),(3,5),(4,5),(2,6),(5,6)]
+
