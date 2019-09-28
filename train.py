39d802e4e21870d06944a2dd0a7a33787c87c3f4
Emily Evans <emilyevans@Emilys-MacBook-Pro-2.local>
2019-09-28 19:38:55 -0400

    getting the project started

--- train.py
+++ train.py
@@ -8,36 +8,6 @@ yard3 = [(1,2), (1,3)]
 yard4 = [(1,2), (1,3), (1,4)]
 yard5 = [(1,2), (1,3), (1,4)]
 
-# (defvar *INIT-STATE-3* ’((*) (a) (b)))
-# (defvar *GOAL-STATE-3* ’((* a b) nil nil))
-
-# (defvar *INIT-STATE-4* ’((*) (a) (b c) (d)))
-# (defvar *GOAL-STATE-4* ’((* a b c d) nil nil nil))
-
-# (defvar *INIT-STATE-5* ’((*) (a) (c b) (d))) ;Note c and b out of order
-# (defvar *GOAL-STATE-5* ’((* a b c d) nil nil nil))
-
-function UNIFORM-COST-SEARCH(problem) returns a solution, or failure
-node ← a node with STATE = problem.INITIAL-STATE, PATH-COST = 0
-frontier ← a priority queue ordered by PATH-COST, with node as the only element explored ← an empty set
-loop do
-if EMPTY?(frontier) then return failure
-node←POP(frontier) /*choosesthelowest-costnodeinfrontier */ if problem.GOAL-TEST(node.STATE) then return SOLUTION(node) add node.STATE to explored
-for each action in problem.ACTIONS(node.STATE) do
-child ←CHILD-NODE(problem,node,action)
-if child.STATE is not in explored or frontier then
-frontier ←INSERT(child,frontier)
-else if child.STATE is in frontier with higher PATH-COST then
-replace that frontier node with child
-
-def possible-actions(lst):
-	pass
-function SIMPLE-PROBLEM-SOLVING-AGENT(percept) returns an action persistent: seq, an action sequence, initially empty
-state, some description of the current world state goal, a goal, initially null
-problem, a problem formulation
-state ← UPDATE-STATE(state,percept) if seq is empty then
-goal ← FORMULATE-GOAL(state)
-problem ←FORMULATE-PROBLEM(state,goal) seq ← SEARCH(problem)
-if seq = failure then return a null action
-action ← FIRST(seq) seq ← REST(seq) return action
-
+def possible-actions(Yard, State):
+	
+	return actions
